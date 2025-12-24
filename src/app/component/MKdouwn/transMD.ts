780949aa4f0a122edcd8337b8ebb949497b83ac5
Mocha-kai <kai@mocha-company.com>
2025-12-24 00:01:59 +0000

    test

--- src/app/component/MKdouwn/transMD.ts
+++ src/app/component/MKdouwn/transMD.ts
@@ -1,17 +1,19 @@
 // lib/posts.ts
 import { remark } from 'remark';
+import remarkGfm from 'remark-gfm'; 
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypeHighlight from 'rehype-highlight';
-import rehypeRaw from 'rehype-raw'; // 🔥 매우 중요!
+import rehypeRaw from 'rehype-raw'; 
 import matter from 'gray-matter';
 
 export async function ConvertMarkdownToHtml(markdownContent: string) {
     const matterResult = matter(markdownContent);
 
     const processedContent = await remark()
+        .use(remarkGfm) 
         .use(remarkRehype, { allowDangerousHtml: true })
-        .use(rehypeRaw) // 🔥 빠져있으면 Markdown 요소가 escape됨
+        .use(rehypeRaw) 
         .use(rehypeHighlight)
         .use(rehypeStringify, { allowDangerousHtml: true })
         .process(matterResult.content);
